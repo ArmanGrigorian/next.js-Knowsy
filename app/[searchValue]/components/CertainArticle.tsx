57dc05b3d2ea0afda0512a9a55b9d36809303c9d
ArmanGrigorian <armangrigorian.work@gmail.com>
2023-11-02 17:58:07 +0400

    little fix
    modified:   app/[searchValue]/components/CertainArticle.tsx
    renamed:    lib/getWikiResults.tsx -> lib/getWikiResults.ts

--- app/[searchValue]/components/CertainArticle.tsx
+++ app/[searchValue]/components/CertainArticle.tsx
@@ -13,33 +13,30 @@ function CertainArticle({ certainArticle }: T_CertainArticleProps) {
 	}
 
 	return (
-		<article className="w-[86%] px-4 pt-3 pb-2 border-[1px] border-neutral-800 rounded-lg">
-			<h3 className="flex items-center gap-3">
-				{certainArticle.thumbnail && (
-					<Image
-						src={info.source}
-						alt={certainArticle.title}
-						width={50}
-						height={40}
-						loading="lazy"
-					/>
-				)}
-				<Link
-					href={`https://en.wikipedia.org/?curid=${certainArticle.pageid}`}
-					target="_blank"
-					className="
-					text-neutral-900
-					 text-[16px]
-					  font-bold
-					   underline
+		<article className="w-[86%] flex justify-between items-center gap-4 px-4 pt-3 pb-2 border-[1px] border-neutral-800 rounded-lg">
+			<div>
+				<h3 className="flex items-center gap-3">
+					<Link
+						href={`https://en.wikipedia.org/?curid=${certainArticle.pageid}`}
+						target="_blank"
+						className="
+						text-neutral-900
+					 	 text-[16px]
+					  	font-bold
+					  underline
 						 underline-offset-2
 						 hover:italic 
 						 hover:opacity-70">
-					{certainArticle.title}
-				</Link>
-			</h3>
+						{certainArticle.title}
+					</Link>
+				</h3>
 
-			<p className="text-justify w-[96%] mx-auto mt-3 pt-1 border-t-[1px] border-neutral-500">{certainArticle.extract}</p>
+				<p className="text-justify">{certainArticle.extract}</p>
+			</div>
+
+			{certainArticle.thumbnail && (
+				<Image src={info.source} alt={certainArticle.title} width={60} height={50} loading="lazy" />
+			)}
 		</article>
 	);
 }
